2784422ab7ed7132bb28a79533fcdc2d31d277e9
Aman Shaikh <amanshaikh7238@gmail.com>
2025-09-18 00:42:24 +0530

    added backend links

--- src/components/AIChatBox.jsx
+++ src/components/AIChatBox.jsx
@@ -25,7 +25,7 @@ const AIChatBox = () => {
     setTyping(true);
 
     try {
-      const res = await fetch("http://localhost:5000/chat", {
+      const res = await fetch("https://portfolio-website-x47k.onrender.com/chat", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ message: question }),
